1aa54e6d42a31728274457b8d0c6d334e6fee0e4
marcosvega91 <marcomoretti0103@gmail.com>
2021-01-24 20:56:42 +0100

    test: force locale to english

--- src/__tests__/help.ts
+++ src/__tests__/help.ts
@@ -4,7 +4,9 @@ const exec = util.promisify(require('child_process').exec);
 
 test('npx unimported --help', async () => {
   // note about `./` path: jest executes the tests from the root directory
-  const { stdout, stderr } = await exec('ts-node src/index.ts --help');
+  const { stdout, stderr } = await exec(
+    `LC_ALL='en' ts-node src/index.ts --help`,
+  );
 
   expect(stderr).toBe('');
   expect(stdout.trim()).toMatchInlineSnapshot(`
