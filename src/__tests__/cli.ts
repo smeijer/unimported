0a1a50654cc602c81c6a7c1c68ce2359c6e0960f
marcosvega91 <marcomoretti0103@gmail.com>
2021-01-25 15:36:31 +0100

    test: add test for update flag

--- src/__tests__/cli.ts
+++ src/__tests__/cli.ts
@@ -6,6 +6,7 @@ import childProcess from 'child_process';
 const mkdir = util.promisify(fs.mkdir);
 const rmdir = util.promisify(fs.rmdir);
 const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
 
 function exec(
   command: string,
@@ -39,35 +40,56 @@ async function createProject(
   return testSpaceDir;
 }
 
-const scenarios = [
-  {
-    description: 'should identify unimported file',
-    files: [
-      { name: 'package.json', content: '{ "main": "index.js" }' },
-      { name: 'index.js', content: `import foo from './foo';` },
-      { name: 'foo.js', content: '' },
-      { name: 'bar.js', content: '' },
-    ],
-    exitCode: 1,
-    output: ['1 unimported files', 'bar.js'],
-  },
-  {
-    description: 'should identify unused dependencies',
-    files: [
-      {
-        name: 'package.json',
-        content:
-          '{ "main": "index.js", "dependencies": { "@test/dependency": "1.0.0" } }',
-      },
-      { name: 'index.js', content: `import foo from './foo';` },
-      { name: 'foo.js', content: '' },
-    ],
-    exitCode: 1,
-    output: ['1 unused dependencies', '@test/dependency'],
-  },
-];
-
 describe('cli integration tests', () => {
+  const scenarios = [
+    {
+      description: 'should identify unimported file',
+      files: [
+        { name: 'package.json', content: '{ "main": "index.js" }' },
+        { name: 'index.js', content: `import foo from './foo';` },
+        { name: 'foo.js', content: '' },
+        { name: 'bar.js', content: '' },
+      ],
+      exitCode: 1,
+      output: ['1 unimported files', 'bar.js'],
+    },
+    {
+      description: 'should identify unused dependencies',
+      files: [
+        {
+          name: 'package.json',
+          content:
+            '{ "main": "index.js", "dependencies": { "@test/dependency": "1.0.0" } }',
+        },
+        { name: 'index.js', content: `import foo from './foo';` },
+        { name: 'foo.js', content: '' },
+      ],
+      exitCode: 1,
+      output: ['1 unused dependencies', '@test/dependency'],
+    },
+    {
+      description: 'everything is used',
+      files: [
+        {
+          name: 'package.json',
+          content:
+            '{ "main": "index.js", "dependencies": { "@test/dependency": "1.0.0" } }',
+        },
+        {
+          name: 'index.js',
+          content: `
+import foo from './foo';
+import bar from './bar';
+`,
+        },
+        { name: 'foo.js', content: '' },
+        { name: 'bar.js', content: 'import test from "@test/dependency"' },
+      ],
+      exitCode: 0,
+      output: [],
+    },
+  ];
+
   scenarios.forEach((scenario) => {
     test(scenario.description, async () => {
       const testProjectDir = await createProject(scenario.files);
@@ -82,6 +104,90 @@ describe('cli integration tests', () => {
         scenario.output.forEach((expectedOutput) => {
           expect(stdout).toEqual(expect.stringContaining(expectedOutput));
         });
+      } finally {
+        await rmdir(testProjectDir, { recursive: true });
+      }
+    });
+  });
+});
+
+describe('cli integration tests with update option', () => {
+  const scenarios = [
+    {
+      description: 'should identify unimported file',
+      files: [
+        { name: 'package.json', content: '{ "main": "index.js" }' },
+        { name: 'index.js', content: `import foo from './foo';` },
+        { name: 'foo.js', content: '' },
+        { name: 'bar.js', content: '' },
+      ],
+      exitCode: 0,
+      output: {
+        ignoreUnresolved: [],
+        ignoreUnimported: ['bar.js'],
+        ignoreUnused: [],
+      },
+    },
+    {
+      description: 'should identify unused dependencies',
+      files: [
+        {
+          name: 'package.json',
+          content:
+            '{ "main": "index.js", "dependencies": { "@test/dependency": "1.0.0" } }',
+        },
+        { name: 'index.js', content: `import foo from './foo';` },
+        { name: 'foo.js', content: '' },
+      ],
+      exitCode: 0,
+      output: {
+        ignoreUnresolved: [],
+        ignoreUnimported: [],
+        ignoreUnused: ['@test/dependency'],
+      },
+    },
+    {
+      description: 'everything is used',
+      files: [
+        {
+          name: 'package.json',
+          content:
+            '{ "main": "index.js", "dependencies": { "@test/dependency": "1.0.0" } }',
+        },
+        {
+          name: 'index.js',
+          content: `
+import foo from './foo';
+import bar from './bar';
+`,
+        },
+        { name: 'foo.js', content: '' },
+        { name: 'bar.js', content: 'import test from "@test/dependency"' },
+      ],
+      exitCode: 0,
+      output: {
+        ignoreUnresolved: [],
+        ignoreUnimported: [],
+        ignoreUnused: [],
+      },
+    },
+  ];
+
+  scenarios.forEach((scenario) => {
+    test(scenario.description, async () => {
+      const testProjectDir = await createProject(scenario.files);
+      const outputFile = path.join(testProjectDir, '.unimportedrc.json');
+      const executable = path.relative(testProjectDir, 'src/index.ts');
+
+      try {
+        const { exitCode } = await exec(`ts-node ${executable} --update`, {
+          cwd: testProjectDir,
+        });
+        expect(exitCode).toBe(scenario.exitCode);
+        const outputFileContent = JSON.parse(
+          await readFile(outputFile, 'utf-8'),
+        );
+        expect(scenario.output).toEqual(outputFileContent);
       } finally {
         await rmdir(testProjectDir, { recursive: true });
       }
