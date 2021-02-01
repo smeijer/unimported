f71928e3e2e1ce65eabebd46cae73068e2cc5161
Michael Peyper <mpeyper7@gmail.com>
2021-02-01 21:39:53 +1100

    fix: await in fs functions to allow errors to be caught

---
+++ src/__tests__/fs.ts
@@ -0,0 +1,157 @@
+import fs from 'fs';
+import path from 'path';
+import { exists, readText, writeText, readJson, writeJson, list } from '../fs';
+
+const testSpaceDir = '.test-space/fs-test';
+
+beforeEach(() => {
+  fs.mkdirSync(testSpaceDir, { recursive: true });
+});
+
+afterEach(() => {
+  fs.rmdirSync(testSpaceDir, { recursive: true });
+});
+
+test('should check if file exists', async () => {
+  const testFileName = path.join(testSpaceDir, 'testFile.txt');
+  await writeText(testFileName, '');
+
+  const fileExists = await exists(testFileName);
+
+  expect(fileExists).toBeTruthy();
+});
+
+test('should check if file exists in cwd', async () => {
+  const testFileName = 'testFile.txt';
+  await writeText(testFileName, '', testSpaceDir);
+
+  const fileExists = await exists(testFileName, testSpaceDir);
+
+  expect(fileExists).toBeTruthy();
+});
+
+test('should check if file does not exists', async () => {
+  const testFileName = path.join(testSpaceDir, 'missing', 'testFile.txt');
+
+  const fileExists = await exists(testFileName);
+
+  expect(fileExists).toBeFalsy();
+});
+
+test('should read text from file', async () => {
+  const testFileName = path.join(testSpaceDir, 'testFile.txt');
+  const expectedText = 'READ ME!';
+  await writeText(testFileName, expectedText);
+
+  const actualText = await readText(testFileName);
+
+  expect(actualText).toBe(expectedText);
+});
+
+test('should read text from file in cwd', async () => {
+  const testFileName = 'testFile.txt';
+  const expectedText = 'READ ME!';
+  await writeText(testFileName, expectedText, testSpaceDir);
+
+  const actualText = await readText(testFileName, testSpaceDir);
+
+  expect(actualText).toBe(expectedText);
+});
+
+test('should gracefully fail to read text from file', async () => {
+  const testFileName = path.join(testSpaceDir, 'missing', 'testFile.txt');
+  const expectedText = '';
+
+  const actualText = await readText(testFileName, testSpaceDir);
+
+  expect(actualText).toBe(expectedText);
+});
+
+test('should gracefully fail to write text to file', async () => {
+  const testFileName = path.join(testSpaceDir, 'missing', 'testFile.txt');
+  const expectedText = 'READ ME!';
+
+  await expect(writeText(testFileName, expectedText)).resolves.toBe(undefined);
+});
+
+test('should read json from file', async () => {
+  const testFileName = path.join(testSpaceDir, 'testFile.txt');
+  const expectedJson = { text: 'READ ME!' };
+  await writeJson(testFileName, expectedJson);
+
+  const actualJson = await readJson(testFileName);
+
+  expect(actualJson).toEqual(expectedJson);
+});
+
+test('should read json from file in cwd', async () => {
+  const testFileName = 'testFile.txt';
+  const expectedJson = { text: 'READ ME!' };
+  await writeJson(testFileName, expectedJson, testSpaceDir);
+
+  const actualJson = await readJson(testFileName, testSpaceDir);
+
+  expect(actualJson).toEqual(expectedJson);
+});
+
+test('should gracefully fail to read json from file', async () => {
+  const testFileName = path.join(testSpaceDir, 'missing', 'testFile.txt');
+  const expectedJson = null;
+
+  const actualJson = await readJson(testFileName, testSpaceDir);
+
+  expect(actualJson).toEqual(expectedJson);
+});
+
+test('should gracefully fail to write json to file', async () => {
+  const testFileName = path.join(testSpaceDir, 'missing', 'testFile.txt');
+  const expectedJson = { text: 'READ ME!' };
+
+  await expect(writeJson(testFileName, expectedJson)).resolves.toBe(undefined);
+});
+
+test('should list files', async () => {
+  await writeText(path.join(testSpaceDir, 'testFile1.txt'), '');
+  await writeText(path.join(testSpaceDir, 'testFile2.txt'), '');
+  await writeText(path.join(testSpaceDir, 'testFile3.txt'), '');
+
+  const files = await list('**/*', testSpaceDir);
+
+  const cleanedFiles = files.map((file) =>
+    file.replace(path.resolve(testSpaceDir), '.'),
+  );
+
+  expect(cleanedFiles).toEqual([
+    './testFile1.txt',
+    './testFile2.txt',
+    './testFile3.txt',
+  ]);
+});
+
+test('should list files matching pattern', async () => {
+  await writeText(path.join(testSpaceDir, 'testFile1.txt'), '');
+  await writeText(path.join(testSpaceDir, 'ignored.txt'), '');
+  await writeText(path.join(testSpaceDir, 'testFile2.txt'), '');
+
+  const files = await list('**/testFile*', testSpaceDir);
+
+  const cleanedFiles = files.map((file) =>
+    file.replace(path.resolve(testSpaceDir), '.'),
+  );
+
+  expect(cleanedFiles).toEqual(['./testFile1.txt', './testFile2.txt']);
+});
+
+test('should list files matching extensions', async () => {
+  await writeText(path.join(testSpaceDir, 'testFile1.txt'), '');
+  await writeText(path.join(testSpaceDir, 'ignored.sh'), '');
+  await writeText(path.join(testSpaceDir, 'testFile2.js'), '');
+
+  const files = await list('**/*', testSpaceDir, { extensions: ['txt', 'js'] });
+
+  const cleanedFiles = files.map((file) =>
+    file.replace(path.resolve(testSpaceDir), '.'),
+  );
+
+  expect(cleanedFiles).toEqual(['./testFile1.txt', './testFile2.js']);
+});
