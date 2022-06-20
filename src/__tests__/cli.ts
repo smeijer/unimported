1b7f3ed31dd60887d781eb64c52d5fc0a3a24ad6
Michael-372 <mpascoe@avantusfederal.com>
2022-06-20 18:43:11 +0000

    finish the test

--- src/__tests__/cli.ts
+++ src/__tests__/cli.ts
@@ -223,6 +223,7 @@ cases(
             import './script-ts.vue'
             import './script-src.vue'
             import './script-setup.vue';
+            import './script-setup-ts.vue';
           `,
         },
         {
@@ -257,15 +258,16 @@ cases(
             </script>
           `,
         },
+        { name: 'script-setup-imported.js', content: '' },
         {
-          name: 'script-setup.vue',
+          name: 'script-setup-ts.vue',
           content: `
-            <script setup lang="ts">
-              import './script-setup-imported';
+            <script setup>
+              import './script-setup-ts-imported';
             </script>
           `,
         },
-        { name: 'script-setup-imported.js', content: '' },
+        { name: 'script-setup-ts-imported.js', content: '' },
         {
           name: 'script-src.vue',
           content: `            
