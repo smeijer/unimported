ef0b43a1fd7c27a39fdeaf7a8e2d2bc205b7f984
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-09-21 22:54:53 -0400

    feat: use cosmiconfig to load config file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import { cosmiconfig } from 'cosmiconfig';
 import { ProcessedResult } from './process';
 import { readJson, writeJson } from './fs';
 import { CliArguments, Context, PackageJson } from './index';
@@ -135,9 +136,8 @@ export async function getConfig(args?: CliArguments): Promise<Config> {
     return cachedConfig;
   }
 
-  const configFile = await readJson<Partial<UnimportedConfig>>(
-    args?.config || CONFIG_FILE,
-  );
+  const cosmiconfigResult = await cosmiconfig('unimported').search();
+  const configFile = (cosmiconfigResult?.config ?? {}) as Partial<UnimportedConfig>;
 
   const unimportedPkg = await readPkgUp({ cwd: __dirname });
 
